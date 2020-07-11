4b2a1fd2eda497d44e9f04bde1c98316bdf352ed
sujatha <jhanavi.bhushan@gmail.com>
2020-07-11 00:15:55 -0400

    Mongo connection added to heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(
   // process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
-  process.env.MONGODB_URI || "mongodb://localhost/reactsearch",
+  process.env.MONGODB_URI || "mongodb://user123:1password@ds119210.mlab.com:19210/heroku_5jr5kkk5",
   {
     useCreateIndex: true,
     useNewUrlParser: true
